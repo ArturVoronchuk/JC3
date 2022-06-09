3293399f9751ed26deb16c3ae83ba1b0027a6106
ArturVoronchuk <103214160+ArturVoronchuk@users.noreply.github.com>
2022-06-09 21:26:50 +0300

    09/06/2022

---
+++ src/DeleteSaves.java
@@ -0,0 +1,26 @@
+package JC_3;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class DeleteSaves {
+    public static void deleting(String path, String files) {
+        File dir = new File("C:/Games/savegames");
+        FilenameFilter filter = new ExtensionFilter(".dat");
+
+        String[] filenames = dir.list(filter);
+        for (String filename : filenames) {
+            String fullFilename = new StringBuilder()
+                    .append(dir)
+                    .append(File.separator)
+                    .append(filename)
+                    .toString();
+
+            File file = new File(fullFilename);
+            Boolean isDeleted = file.delete();
+            System.out.println("Фaйл yдaлeн: " + fullFilename + isDeleted.toString());
+            System.out.println();
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,13 @@ public class Main {
         CreateFile.createFile();
         StringB.logsSaved();
         WriteFile.fileWr();
+        GameProgress gameProgress1 = new GameProgress(35, 11, 80, 39.5);
+        GameProgress gameProgress2 = new GameProgress(15, 23, 13, 15.5);
+        GameProgress gameProgress3 = new GameProgress(1330, 19, 100, 444.5);
+        SaveGames.saveGame("C:/Games/savegames", gameProgress1);
+        SaveGames.saveGame("C:/Games/savegames", gameProgress2);
+        SaveGames.saveGame("C:/Games/savegames", gameProgress3);
+        ZipFiles.zipFiles("C:/Games/savegames/zip.zip", "C:/Games/savegames/save1.dat");
+        DeleteSaves.deleting("C:/Games/savegames/","save1.dat");
     }
 }
